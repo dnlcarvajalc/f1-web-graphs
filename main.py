34a9c43093740c8a741f5ee1cc15d055cbb81036
Daniel Carvajal <dnlcarvajalc@gmail.com>
2023-10-19 20:27:30 -0500

    use black in every file

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import driver_lap_times
 
 drivers_for_lap_times = {
-    "driver1":{
-        "name" :"perez",
-        "color":"b",
-        "alias":"ChequitoBB"
-    },
-    "driver2":{
-        "name" :"max_verstappen",
-        "color":"r",
-        "alias":"Srto Maximiliano"
-    },
-    "driver3":{
-        "name" :"sainz",
-        "color":"y",
-        "alias":"Il Matador"
-    },
-    "driver4":{
-        "name" :"russell",
-        "color":"g",
-        "alias":"Mr Saturday"
-    }
+    "driver1": {"name": "perez", "color": "b", "alias": "ChequitoBB"},
+    "driver2": {"name": "max_verstappen", "color": "r", "alias": "Srto Maximiliano"},
+    "driver3": {"name": "sainz", "color": "y", "alias": "Il Matador"},
+    "driver4": {"name": "russell", "color": "g", "alias": "Mr Saturday"},
 }
 
 if __name__ == "__main__":
-    driver_lap_times.plot_lap_times(year_race=2023, round_race=15, driver_list=drivers_for_lap_times)
+    driver_lap_times.plot_lap_times(
+        year_race=2023, round_race=15, driver_list=drivers_for_lap_times
+    )
